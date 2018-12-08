25d0288f16687b433319edeb94b5c74fe83e8d25
Sean Dietrich <sean@kanopistudios.com>
2018-12-07 22:58:08 -0800

    Added contact form

--- src/components/contact-form.js
+++ src/components/contact-form.js
@@ -2,13 +2,14 @@ import React from 'react'
 
 const ContactForm = (props) => (
   <div>
-    <form name="contact" netlify>
+    <form name="contact" method="POST" netlify>
       <p>
         <label>Name <input type="text" name="name" /></label>
       </p>
       <p>
         <label>Email <input type="email" name="email" /></label>
       </p>
+      <div data-netlify-recaptcha></div>
       <p>
         <button type="submit">Send</button>
       </p>
